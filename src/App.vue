48f4a2894cfb5778d643f7536495151b4be78ccc
Francisco Fernandes <fmfernandes@primeit.pt>
2020-02-07 14:45:32 +0000

    add sass support and implement a navbar

--- src/App.vue
+++ src/App.vue
@@ -4,29 +4,32 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    <div class="container bg-dark">
+      <nav class="navbar navbar-expand-sm justify-content-sm-center">
+        <div class="cross-btn-container">
+          <button
+            class="navbar-toggler navbar-toggler-right collapsed"
+            type="button"
+            data-toggle="collapse"
+            data-target="#collapsableNavbar"
+          >
+            <span></span>
+            <span></span>
+            <span></span>
+          </button>
+        </div>
+        <div class="collapse navbar-collapse" id="collapsableNavbar">
+          <div class="navbar-nav text-center">
+            <router-link to="/">Home</router-link>|
+            <router-link to="/about">About</router-link>
+          </div>
+        </div>
+      </nav>
+    </div>
+    <router-view />
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+<style lang='scss'>
+@import "./styles.scss";
 </style>
