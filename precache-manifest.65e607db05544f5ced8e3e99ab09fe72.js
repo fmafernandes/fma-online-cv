f26a34fbaeca5057878db399719b09efa3aaaeb8
Francisco Fernandes <fmfernandes@primeit.pt>
2020-02-11 11:26:32 +0000

    maybe this time

---
+++ precache-manifest.65e607db05544f5ced8e3e99ab09fe72.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74f2bf9591ae1bc4ed30",
+    "url": "/css/about.dff5017f.css"
+  },
+  {
+    "revision": "84e6366f1669703b92c8",
+    "url": "/css/app.c2d07701.css"
+  },
+  {
+    "revision": "be38c2932fd4e8dbcb32",
+    "url": "/css/chunk-vendors.f6f30965.css"
+  },
+  {
+    "revision": "d751de38dec65daee33892b6378a3d01",
+    "url": "/fonts/AvenirLTStd-Black.d751de38.otf"
+  },
+  {
+    "revision": "d751de38dec65daee33892b6378a3d01",
+    "url": "/fonts/AvenirLTStd-Black.otf"
+  },
+  {
+    "revision": "4f995fa49446998983e05df9994dc96c",
+    "url": "/fonts/AvenirLTStd-Medium.4f995fa4.otf"
+  },
+  {
+    "revision": "4f995fa49446998983e05df9994dc96c",
+    "url": "/fonts/AvenirLTStd-Medium.otf"
+  },
+  {
+    "revision": "6b42e5fcb3a116c7f522ff57bc7a6b5e",
+    "url": "/img/images/fmaf-logo.png"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/images/logo.png"
+  },
+  {
+    "revision": "ad6c72a0592e37a55b6c3520736488a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "74f2bf9591ae1bc4ed30",
+    "url": "/js/about.51641e60.js"
+  },
+  {
+    "revision": "84e6366f1669703b92c8",
+    "url": "/js/app.9fbbf336.js"
+  },
+  {
+    "revision": "be38c2932fd4e8dbcb32",
+    "url": "/js/chunk-vendors.148f2d68.js"
+  },
+  {
+    "revision": "3bb962ba6e8b72e2fcf72c20699342aa",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
